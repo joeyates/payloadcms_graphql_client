c343413ae710043b058dfe977851552873b8babb
Joe Yates <joe.g.yates@gmail.com>
2025-04-28 09:56:41 +0200

    Implement a renderer for rich text fields

---
+++ lib/payloadcms_graphql_client/rich_text.ex
@@ -0,0 +1,139 @@
+defmodule PayloadcmsGraphqlClient.RichText do
+  @moduledoc """
+  Conveniences for handling Payload CMS rich text fields.
+  """
+
+  @custom_renderer_key %{
+    "inlineBlock" => :inline_block,
+    "upload" => :upload
+  }
+  @custom_renderer_required Map.keys(@custom_renderer_key)
+
+  def to_html(%{root: root}, options \\ %{}) do
+    render(root, options)
+  end
+
+  def render(%{type: "root", children: children}, options) do
+    children
+    |> Enum.flat_map(&render(&1, options))
+    |> Enum.join()
+  end
+
+  def render(%{type: "heading", tag: tag} = node, options) do
+    case renderer(:heading, options) do
+      nil ->
+        ["<#{tag}>"] ++ Enum.flat_map(node.children, &render(&1, options)) ++ ["</#{tag}>"]
+
+      renderer ->
+        renderer.(node, options)
+    end
+  end
+
+  def render(%{type: "list", tag: "ul"} = node, options) do
+    case renderer(:unordered_list, options) do
+      nil ->
+        items =
+          Enum.flat_map(
+            node.children,
+            fn list_item ->
+              ["<li>"] ++
+                Enum.flat_map(list_item.children, &render(&1, options)) ++
+                ["</li>"]
+            end
+          )
+
+        ["<ul>"] ++ items ++ ["</ul>"]
+
+      renderer ->
+        renderer.(node, options)
+    end
+  end
+
+  def render(%{type: "paragraph"} = node, options) do
+    case renderer(:paragraph, options) do
+      nil ->
+        ["<p>"] ++ Enum.flat_map(node.children, &render(&1, options)) ++ ["</p>"]
+
+      renderer ->
+        renderer.(node, options)
+    end
+  end
+
+  def render(%{type: "link", fields: %{url: url, newTab: true}, children: children}, options) do
+    [~s(<a href="#{url}" target="_blank">)] ++
+      Enum.flat_map(children, &render(&1, options)) ++ ["</a>"]
+  end
+
+  def render(%{type: "text", text: text}, _options) do
+    [text]
+  end
+
+  def render(%{type: "horizontalrule"}, _options) do
+    ["<hr>"]
+  end
+
+  def render(%{type: type} = node, options) when type in @custom_renderer_required do
+    name = Map.fetch!(@custom_renderer_key, type)
+
+    case renderer!(name, options) do
+      {:ok, renderer} ->
+        renderer.(node, options)
+
+      {:error, error} ->
+        raise """
+        Error rendering #{name} node: #{inspect(node)}
+
+        A custom renderer is required for '#{type}' nodes.
+
+        Error: #{error}
+        """
+    end
+  end
+
+  def render(node, options) do
+    message =
+      """
+      No render/2 clause matching for node: #{inspect(node)}
+
+      Options: #{inspect(options)}
+      """
+
+    raise message
+  end
+
+  defp renderer!(name, %{renderers: renderers} = options) do
+    renderer = renderer(name, options)
+
+    if renderer do
+      {:ok, renderer}
+    else
+      {
+        :error,
+        """
+        No `#{name}` function supplied in options.renders
+
+        Supplied renderers:
+        #{inspect(Map.keys(renderers))}
+        """
+      }
+    end
+  end
+
+  defp renderer!(name, options) do
+    {
+      :error,
+      """
+      Can't find renderer for `#{name}` as
+      no `:renderers` were supplied in options:
+
+      options: #{inspect(Map.keys(options))}
+      """
+    }
+  end
+
+  defp renderer(name, %{renderers: renderers}) do
+    renderers[name]
+  end
+
+  defp renderer(_name, _options), do: nil
+end
