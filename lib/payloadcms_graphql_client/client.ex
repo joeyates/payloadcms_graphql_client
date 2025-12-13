c11893d8f0e0d0c99fa23c2be304650af0ca6b61
Joe Yates <joe.g.yates@gmail.com>
2025-12-13 18:03:23 +0100

    Optionally add HTTP basic auth to the Authorization header

--- lib/payloadcms_graphql_client/client.ex
+++ lib/payloadcms_graphql_client/client.ex
@@ -8,11 +8,12 @@ defmodule PayloadcmsGraphqlClient.Client do
   def fetch!(%Query{} = query) do
     query_string = to_string(query)
     timeout = Application.get_env(:payloadcms_graphql_client, :timeout, 30_000)
+    headers = headers()
 
     Req.post!(
       query.endpoint,
       json: %{query: query_string},
-      headers: headers(),
+      headers: headers,
       connect_options: [timeout: timeout],
       decode_json: [keys: :atoms]
     )
@@ -23,15 +24,54 @@ defmodule PayloadcmsGraphqlClient.Client do
       {"Content-Type", "application/json"}
     ]
 
-    api_key = Application.get_env(:payloadcms_graphql_client, :api_key)
+    headers ++ autorization_header()
+  end
 
-    if api_key do
-      collection_slug =
-        Application.get_env(:payloadcms_graphql_client, :api_key_collection_slug, "users")
+  defp autorization_header() do
+    auth_values = Enum.filter([basic_auth_value(), api_key_value()], & &1)
 
-      headers ++ [{"Authorization", "#{collection_slug} API-Key #{api_key}"}]
-    else
-      headers
+    case auth_values do
+      [] -> []
+      values -> [{"Authorization", Enum.join(values, ", ")}]
     end
+  end
+
+  defp api_key_value() do
+    case api_key() do
+      nil ->
+        nil
+
+      api_key ->
+        collection_slug =
+          Application.get_env(:payloadcms_graphql_client, :api_key_collection_slug, "users")
+
+        "#{collection_slug} API-Key #{api_key}"
+    end
+  end
+
+  defp basic_auth_value() do
+    case {basic_auth_user(), basic_auth_password()} do
+      {nil, _} ->
+        nil
+
+      {_, nil} ->
+        nil
+
+      {user, password} ->
+        encoded = Base.encode64("#{user}:#{password}")
+        "Basic #{encoded}"
+    end
+  end
+
+  defp api_key() do
+    Application.get_env(:payloadcms_graphql_client, :api_key)
+  end
+
+  defp basic_auth_password() do
+    Application.get_env(:payloadcms_graphql_client, :basic_auth_password)
+  end
+
+  defp basic_auth_user() do
+    Application.get_env(:payloadcms_graphql_client, :basic_auth_user)
   end
 end
